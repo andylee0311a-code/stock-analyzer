51b1d6ae566f5a527d45735e9fd1f8a29b96ce5b
unknown <andylee0311a@google.com>
2026-04-10 14:30:42 +0800

    fix css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css' // 👈 確認有沒有這一行！沒有的話 CSS 就不會進到網頁裡
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}", // 👈 這行超級重要！它告訴 Tailwind 去掃描 src 資料夾下所有的 jsx 檔案
   ],
   theme: {
     extend: {},
