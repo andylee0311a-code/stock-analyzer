189026c3e003abb92f406aacc208a5121e58dc20
unknown <andylee0311a@google.com>
2026-04-13 15:30:52 +0800

    Migrate to Tailwind CSS v4 Vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 👈 1. 引入剛剛裝的新套件
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // 👈 2. 把 tailwind 加入 plugin 陣列裡
+  ],
 })
