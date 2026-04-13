ed036e838a9e0085c3d9baea695b26d3af267cdd
unknown <andylee0311a@google.com>
2026-04-13 15:35:16 +0800

    Fix React DOM createRoot initialization

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css' // 👈 確認有沒有這一行！沒有的話 CSS 就不會進到網頁裡
+import './index.css' // 確保 Tailwind CSS 被載入
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
